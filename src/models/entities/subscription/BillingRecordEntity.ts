a65ed401ae7a67f50ff08be4b35832842495a4dd
hexley21 <a.kupreishvili.21@gmail.com>
2024-03-06 17:26:03 +0400

    feat(subscription): add subscription entities

---
+++ src/models/entities/subscription/BillingRecordEntity.ts
@@ -0,0 +1,74 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({
+    schema: "subscription",
+    name: "billing_record"
+})
+export default class BillingRecordEntity {
+    
+    constructor(company_id: string, plan_id:  string, plan_start?: Date, user_count?: number, files_uploaded?: number) {
+        this.company_id = company_id
+        this.plan_id = plan_id
+        
+        if (plan_start) this.plan_start = plan_start
+        if (user_count) this.user_count = user_count
+        if (files_uploaded) this.files_uploaded = files_uploaded
+    }
+
+    
+    @PrimaryGeneratedColumn("uuid")
+    public id!: string;
+
+    @Column({
+        type: "varchar",
+        name: "company_id",
+        nullable: false
+    })
+    public company_id!: string;
+
+    @Column({
+        type: "varchar",
+        name: "plan_id",
+        nullable: false
+    })
+    public plan_id!: string;
+
+    @Column({
+        type: "date",
+        name: "plan_start",
+        nullable: false
+    })
+    public plan_start!: Date;
+
+    @Column({
+        type: "date",
+        name: "plan_end",
+        generatedType: "STORED",
+        asExpression: `plan_start + INTERVAL '1 month'`,
+        nullable: false
+    })
+    public plan_end!: Date;
+
+    @Column({
+        type: "date",
+        name: "billed_at",
+        nullable: true
+    })
+    public billed_at?: Date;
+
+    @Column({
+        type: "integer",
+        name: "user_count",
+        nullable: false
+    })
+    public user_count!: number;
+
+    @Column({
+        type: "integer",
+        name: "files_uploaded",
+        nullable: false
+    })
+    public files_uploaded!: number;
+
+}
---
+++ src/models/entities/subscription/PlanEntity.ts
@@ -0,0 +1,66 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity({
+    schema: "subscription",
+    name: "plan"
+})
+export default class PlanEntity {
+
+    constructor(price: number, name: string, file_limit?: number, user_limit?: number, file_price?: number, user_price?: number) {
+        this.price = price;
+        this.name = name;
+        this.file_limit = file_limit;
+        this.user_limit = user_limit;
+        this.file_price = file_price;
+        this.user_price = user_price;
+    }
+
+
+    @PrimaryGeneratedColumn("increment")
+    public id!: number
+
+    
+    @Column({
+        type: "money",
+        name: "price",
+        nullable: false
+    })
+    public price!: number;
+
+    @Column({
+        type: "varchar",
+        name: "name",
+        nullable: false
+    })
+    public name!: string;
+
+    @Column({
+        type: "integer",
+        name: "file_limit",
+        nullable: true
+    })
+    public file_limit?: number;
+
+    @Column({
+        type: "integer",
+        name: "user_limit",
+        nullable: true
+    })
+    public user_limit?: number;
+
+    @Column({
+        type: "money",
+        name: "file_price",
+        nullable: true
+    })
+    public file_price?: number;
+
+    @Column({
+        type: "money",
+        name: "user_price",
+        nullable: true
+    })
+    public user_price?: number;
+
+}
