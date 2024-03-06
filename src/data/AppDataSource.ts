c03e0d68245dd64aeeff616a1d3b9b188c870f96
hexley21 <a.kupreishvili.21@gmail.com>
2024-03-06 18:18:29 +0400

    feat(orm)t: add basic entities to data-source

--- src/data/AppDataSource.ts
+++ src/data/AppDataSource.ts
@@ -1,5 +1,16 @@
 import DbHandler from "../common/handlers/DbHandler.js";
 
+import CountriesEntity from "../models/entities/CountriesEntity.js";
+import IndustriesEntity from "../models/entities/IndustriesEntity.js";
+
+import UserEntity from "../models/entities/users/UserEntity.js";
+import AuthEntity from "../models/entities/users/AuthEntity.js";
+import CompanyEntity from "../models/entities/users/CompanyEntity.js";
+import EmployeeEntity from "../models/entities/users/EmployeeEntity.js";
+
+import BillingRecordEntity from "../models/entities/subscription/BillingRecordEntity.js";
+import PlanEntity from "../models/entities/subscription/PlanEntity.js";
+
 
 export default new DbHandler({
     type: "postgres",
@@ -10,7 +21,7 @@ export default new DbHandler({
     database: process.env.DB_NAME!,
     synchronize: false,
     logging: true,
-    entities: [],
+    entities: [ IndustriesEntity, CountriesEntity, UserEntity, AuthEntity, CompanyEntity, EmployeeEntity, BillingRecordEntity, PlanEntity ],
     subscribers: [],
     migrations: []
 });
