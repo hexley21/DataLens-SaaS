1eea818e9f408dde5304043b0071809ebc5cfc1d
hexley21 <a.kupreishvili.21@gmail.com>
2024-03-08 21:09:01 +0400

    fix(encryption): resolve incorrect promise

--- src/services/BasicEncriptionService.ts
+++ src/services/BasicEncriptionService.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import IEncriptionService from "../common/base/IEncriptionService.js";
 
 import crypto from "crypto";
@@ -7,13 +8,24 @@ export default class BasicEncriptionService implements IEncriptionService {
     saltLength: number = parseInt(process.env.SALT_LENGTH!);
 
 
-    public async encryptPassword(password: string, salt?: string, length?: number | undefined): Promise<string | never> {
-    if (!password) throw Error("Null password")
+    public async encryptPassword(password?: string, salt?: string, length?: number | undefined): Promise<string> | never {
+        if (!password) throw Error("Null password")
         if (password.includes(" ")) throw Error("Password includes spaces")
 
-        if (!salt) salt = this.getSalt(length)
+        return new Promise<string>((resolve, reject) => {
+            crypto.pbkdf2(
+                password,
+                salt ?? this.getSalt(length),
+                1000,
+                (length ?? this.saltLength) / 2,
+                "sha512",
+                (err, key) => {
+                    if (err) reject(err);
+                    else resolve(key.toString('hex'));
+            })
+        })
+        
 
-        return crypto.pbkdf2Sync(password, salt, 1000, (length ?? this.saltLength) / 2, "sha512").toString("hex");
     }
 
     public getSalt(length?: number): string {
